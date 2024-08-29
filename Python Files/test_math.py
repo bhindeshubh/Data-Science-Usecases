dfb55e39388f1490639e7b4903b34b70bbfd8d08
bhindeshubh <bhindeshubh@gmail.com>
2024-08-29 10:04:13 +0530

    Added xyz feature, updated structure for llm

---
+++ Python Files/test_math.py
@@ -0,0 +1,30 @@
+
+# Create a function to add two numbers
+def add(num1, num2):
+    """This is the function to add 2 numbers"""
+    s = num1 + num2
+    return(s)
+
+# Create a function to subtract two numbers
+def sub(num1, num2):
+    """This is the function to subtract 2 numbers"""
+    subtr = num1 - num2
+    return(subtr)
+
+# Create a function to multiply two numbers
+def mult(num1, num2):
+    """This is the function to multiply 2 numbers"""
+    p = num1*num2
+    return(p)
+
+# Create a function to divide two numbers
+def div(num1, num2):
+    """This is the function to divide 2 numbers"""
+    d = num1/num2
+    return(d)
+
+# Create a function to find exponent of a number
+def exp(num1, num2):
+    """This is the function to find exponent of a number"""
+    e = num1**num2
+    return(e)
